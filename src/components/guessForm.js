f7e2ecddd1af3aceef1c4045a1f5b248a2797be8
Logan Wang <loganxwang@hotmail.com>
2019-09-11 13:51:38 +1000

    added text box reset funationality for usability

--- src/components/guessForm.js
+++ src/components/guessForm.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './guessForm.css';
 export default function GuessForm(props) {
   return (
-    <form onSubmit={e => {
+    <form id="guessInput" onSubmit={e => {
       e.preventDefault();
       props.onSubmit(Number(e.target.userGuess.value));
+      document.getElementById("guessInput").reset();
     }}>
       <input type="text" name="userGuess" id="userGuess"
         className="text" maxLength="3" autoComplete="off"
